facb8fad099e5d0efa97c773c35aad9b4a2734db
Marmin <marmin.dev@gmail.com>
2023-03-23 14:57:31 +0900

    template impl

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from booker import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('booker/',include('booker.urls')),
+    path('', views.index)
+]
