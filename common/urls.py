264c731786c9582f8462d85d3d11585e990427ca
Marmin <marmin.dev@gmail.com>
2023-03-26 13:50:12 +0900

    password reset implemented

--- common/urls.py
+++ common/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     ),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('signup/', views.signup ,name='signup'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
-from .forms import UserForm
+from .forms import UserForm, PasswordResetForm
 from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 
 
 def signup(request):
+    """
+    회원가입
+    """
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -16,3 +21,34 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form':form})
+
+class PasswordResetView(auth_views.PasswordResetView):
+    """
+    비밀번호 초기화 - 사용자ID, email 입력
+    """
+    template_name = 'common/password_reset.html'
+    # success_url = reverse_lazy('password_reset_done')
+    form_class = PasswordResetForm
+    # email_template_name = 'common/password_reset_email.html'
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    """
+    비밀번호 초기화 - 메일 전송 완료
+    """
+    template_name = 'common/password_reset_done.html'
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    """
+    비밀번호 초기화 - 새로운 비밀번호 입력
+    """
+    template_name = 'common/password_reset_confirm.html'
+    success_url = reverse_lazy('login')
+
+
+def page_not_found(request, exception):
+    """
+    404 Page not found
+    """
+    return render(request, 'common/404.html', {})
